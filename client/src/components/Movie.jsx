2bef327df3b4861f3737f123783cfaa7ca4b96b2
Benjamin Conway <benrconway84@gmail.com>
2017-11-06 23:43:55 +0000

    Basic structure achieved

---
+++ client/src/components/Movie.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Movie extends React.Component {
+
+  render() {
+
+  }
+
+}
+
+
+export default Movie;
---
+++ client/src/components/MovieScaffold.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class MovieScaffold extends React.Components{
+
+  render() {
+
+  }
+}
+
+
+export default MovieScaffold;
